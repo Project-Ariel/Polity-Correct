a532da0e827b3508f05bd1742626b40a145666ba
Roei Birger <rbirger@Aternity.com>
2021-11-29 10:41:56 +0200

    Init Firebase DB

--- app/src/main/java/com/example/polity_correct/MainActivity.java
+++ app/src/main/java/com/example/polity_correct/MainActivity.java
@@ -3,18 +3,25 @@ package com.example.polity_correct;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText txtAccountID, txtPass;
+    //FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    //    db = FirebaseFirestore.getInstance();
         txtAccountID = (EditText)findViewById(R.id.txtAccountID);
         txtPass = (EditText)findViewById(R.id.txtPass);
         String AccountID = txtAccountID.toString();
