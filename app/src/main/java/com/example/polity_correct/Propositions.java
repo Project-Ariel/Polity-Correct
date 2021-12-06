ac10640c2a17330950b8d2e6e3db29196db4d3c7
Reut Maslansky <rmaslansky@aternity.com>
2021-12-06 22:26:36 +0200

    add Vote activity, and add listener for vote button

--- app/src/main/java/com/example/polity_correct/Propositions.java
+++ app/src/main/java/com/example/polity_correct/Propositions.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 public class Propositions extends AppCompatActivity {
 
     private ImageView logo;
+    private Button[] votesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +26,33 @@ public class Propositions extends AppCompatActivity {
                 openHomePage();
             }
         });
+
+        //vote listener
+        int[] ids = new int[]{R.id.voteBtn0, R.id.voteBtn1, R.id.voteBtn2, R.id.voteBtn3, R.id.voteBtn4, R.id.voteBtn5, R.id.voteBtn6,
+                R.id.voteBtn7, R.id.voteBtn8, R.id.voteBtn9, R.id.voteBtn10, R.id.voteBtn11};
+        votesButton = new Button[ids.length];
+        for (int i = 0; i < votesButton.length; i++) {
+            votesButton[i] = (Button) findViewById(ids[i]);
+            int finalI = i;
+            votesButton[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onClickVote(finalI);
+                }
+            });
+        }
     }
 
     public void openHomePage() {
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
+    }
+
+    public void onClickVote(int id) {
+        Intent next = new Intent(this, Vote.class);
+        Bundle b = new Bundle();
+        b.putString("voteID", String.valueOf(id));
+        next.putExtras(b);
+        startActivity(next);
     }
 }
---
+++ app/src/main/java/com/example/polity_correct/Vote.java
@@ -0,0 +1,26 @@
+package com.example.polity_correct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Vote extends AppCompatActivity {
+
+    private TextView voteId;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vote);
+
+        voteId = (TextView) findViewById(R.id.voteID);
+        Bundle b = getIntent().getExtras();
+        if (b != null) {
+            voteId.setText("Vote ID:" + (b.getString("voteID")));
+        }
+    }
+}
