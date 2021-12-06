643f980b21e61d6a45f16ae5ab711f432c0d7ac5
Reut Maslansky <rmaslansky@aternity.com>
2021-12-06 21:39:16 +0200

    Propositions basic design

--- app/src/main/java/com/example/polity_correct/Propositions.java
+++ app/src/main/java/com/example/polity_correct/Propositions.java
@@ -2,13 +2,33 @@ package com.example.polity_correct;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Propositions extends AppCompatActivity {
+
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.propositions);
+
+        // logo listener
+        logo = (ImageView) findViewById(R.id.logo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomePage();
+            }
+        });
+    }
+
+    public void openHomePage() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
