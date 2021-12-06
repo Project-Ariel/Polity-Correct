2276900846603e1f1963856ee5af9b35df284c04
Roei Birger <rbirger@Aternity.com>
2021-12-06 21:59:46 +0200

    Update DB template
    on login screen

--- app/src/main/java/com/example/polity_correct/Login.java
+++ app/src/main/java/com/example/polity_correct/Login.java
@@ -1,26 +1,36 @@
 package com.example.polity_correct;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Login extends AppCompatActivity {
 
-    EditText txtAccountID, txtPass;
+    EditText txtAccountMail, txtPass;
     Button login;
 
-    //FirebaseFirestore db;
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-    //    db = FirebaseFirestore.getInstance();
-        txtAccountID = (EditText)findViewById(R.id.txtAccountID);
+        db = FirebaseFirestore.getInstance();
+        txtAccountMail = (EditText)findViewById(R.id.txtAccountMail);
         txtPass = (EditText)findViewById(R.id.txtPass);
 
         //home page listener
@@ -37,8 +47,44 @@ public class Login extends AppCompatActivity {
     public void onClickLogin() {
         Intent next = new Intent(Login.this,Home.class);
         Bundle b = new Bundle();
-        b.putString("AccountID", txtAccountID.getText().toString());
+        b.putString("AccountMail", txtAccountMail.getText().toString());
+        b.putString("Pass", txtPass.getText().toString());
         next.putExtras(b);
         startActivity(next);
+    }
+
+    public void updateDBOnClick(View view) {
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("fullName", "my name");
+        user.put("gender", "male");
+        user.put("mail", txtAccountMail.getText().toString());
+        user.put("password", txtPass.getText().toString());
+        user.put("politicalGroupKey", "LoveMe");
+        user.put("userType", 1);
+        user.put("yearOfBirth", 1234);
+
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    private static final String TAG = " ";
+
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    private final Object TAG = " ";
+
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w((String) TAG, "Error adding document", e);
+                    }
+                });
+
     }
 }
