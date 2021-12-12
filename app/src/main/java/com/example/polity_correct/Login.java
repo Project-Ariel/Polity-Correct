c88671063f1a581f7bff51879b73494020f78d03
Roei Birger <rbirger@Aternity.com>
2021-12-12 16:49:03 +0200

    Sign-up with Firebase

--- app/src/main/java/com/example/polity_correct/Login.java
+++ app/src/main/java/com/example/polity_correct/Login.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -28,26 +29,59 @@ import java.util.Map;
 public class Login extends AppCompatActivity {
 
     EditText txtAccountMail, txtPass;
+    String mail, pass;
+
     FirebaseFirestore db;
+    static private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // User is signed in
+            FirebaseAuth.getInstance().signOut();
+        }
+
         db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
         txtAccountMail = (EditText)findViewById(R.id.textUsermail_login);
         txtPass = (EditText)findViewById(R.id.textPassword_login);
+
+        mail= txtAccountMail.getText().toString();
+        pass= txtPass.getText().toString();
     }
 
     //open Home page
     public void onClickLogin(View view) {
-        Intent next = new Intent(Login.this, HomeCitizen.class);
-        Bundle b = new Bundle();
-        b.putString("AccountMail", txtAccountMail.getText().toString());
-        b.putString("Pass", txtPass.getText().toString());
-        next.putExtras(b);
-        startActivity(next);
+        mail= txtAccountMail.getText().toString();
+        pass= txtPass.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(mail, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Intent next = new Intent(Login.this, HomeCitizen.class);
+                            Bundle b = new Bundle();
+                            b.putString("AccountMail", txtAccountMail.getText().toString());
+                            b.putString("Pass", txtPass.getText().toString());
+                            next.putExtras(b);
+                            startActivity(next);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void onClickRegister(View view) {
