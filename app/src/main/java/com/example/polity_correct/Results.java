744e121ac4bff8bb137760708b9b57bc3595f642
Reut Maslansky <rmaslansky@aternity.com>
2021-12-24 12:19:57 +0200

    Add propositions to drop-list

--- app/src/main/java/com/example/polity_correct/Results.java
+++ app/src/main/java/com/example/polity_correct/Results.java
@@ -1,14 +1,60 @@
 package com.example.polity_correct;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class Results extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Results extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner dropdown;
+    private CardView result;
+
+    ArrayList<String> titles= new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results);
+
+        TextView title = (TextView) findViewById(R.id.title_page);
+        title.setText("תוצאות האמת");
+
+        dropdown = (Spinner) findViewById(R.id.spinnerProp);
+
+        Intent i = getIntent();
+        if (i != null) {
+            ArrayList<Proposition> propositions = (ArrayList<Proposition>) i.getSerializableExtra("propositions");
+            for (Proposition p : propositions) {
+                titles.add(p.getTitle());
+            }
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, titles);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(this);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        System.out.println(parent.getItemAtPosition(position).toString());
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        System.out.println("here");
+    }
+
+    public void openResultsView(View view) {
+        result = (CardView) findViewById(R.id.result_view);
+        result.setVisibility(View.VISIBLE);
     }
 }
