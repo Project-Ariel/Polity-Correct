8163b30c3c43e2ee9738933d0f4dd5e12f18d1f1
yaara <yaarabarak45620@gmail.com>
2021-12-27 17:16:22 +0200

    delete copy constructor

--- app/src/main/java/com/example/polity_correct/ParliamentMember.java
+++ app/src/main/java/com/example/polity_correct/ParliamentMember.java
@@ -4,13 +4,8 @@ import java.util.Date;
 
 public class ParliamentMember extends User{
 
-    public ParliamentMember(String userName, String password, String mail, int yearOfBirth, int gander, UserType userType, String pg) {
+    public ParliamentMember(String userName, String password, String mail, Long yearOfBirth, int gander, UserType userType, String pg) {
         super(userName, password, mail, yearOfBirth, gander, userType, pg);
-    }
-
-    //copy constructor
-    public ParliamentMember(User user){
-        super(user.getUserName(), user.getPassword(), user.getMail(), user.getYearOfBirth(), user.getGander(), user.getUserType(), user.getKey_pg());
     }
 
 
