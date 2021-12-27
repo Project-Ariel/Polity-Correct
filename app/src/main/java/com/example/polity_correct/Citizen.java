001794c1c818da646c6ea01e2b385f1ac1e4f0fc
yaara <yaarabarak45620@gmail.com>
2021-12-27 17:16:04 +0200

    update vote function

--- app/src/main/java/com/example/polity_correct/Citizen.java
+++ app/src/main/java/com/example/polity_correct/Citizen.java
@@ -10,6 +10,7 @@ import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Date;
 
@@ -20,14 +21,12 @@ public class Citizen extends User{
 
     private String id;
 
-    public Citizen(String id, String userName, String password, String mail, int yearOfBirth,int gender, UserType userType, String pg) {
+    public Citizen(String id, String userName, String password, String mail, Long yearOfBirth, int gender, UserType userType, String pg) {
         super(userName, password, mail, yearOfBirth,gender, userType, pg);
         this.id=id;
 
     }
-    public Citizen(User user){
-        super(user.getUserName(), user.getPassword(), user.getMail(), user.getYearOfBirth(), user.getGander(), user.getUserType(), user.getKey_pg());
-    }
+    public  Citizen(){}
 
     public String getId() {
         return id;
@@ -37,12 +36,37 @@ public class Citizen extends User{
         this.id = id;
     }
 
-    public void Vote(int proposition_key, int grade, StatusVote status, Timestamp date){
+    public void Vote(Citizen citizen_details,String proposition_key, double grade, StatusVote status, Timestamp date){
+        FirebaseFirestore db;
+        FirebaseAuth mAuth;
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         Map<String, Object> vote = new HashMap<>();
-        vote.put("user_id", id);
+        vote.put("user_id", mAuth.getCurrentUser().getUid());
+        vote.put("user_name",citizen_details.getUserName());
         vote.put("proposition_key", proposition_key);
         vote.put("user_choice", status);
         vote.put("vote_grade", grade);
         vote.put("vote_date", date);
+
+        db.collection("Votes")
+                .add(vote)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    private static final String TAG = " ";
+
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    private final Object TAG = " ";
+
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w((String) TAG, "Error adding document", e);
+                    }
+                });
     }
 }
