c7524d5e897714f87d848b3fb7a3f2c9ba6558b3
Reut Maslansky <rmaslansky@aternity.com>
2021-12-27 18:31:00 +0200

    Vote by User function "Citizen.vote"

--- app/src/main/java/com/example/polity_correct/Vote.java
+++ app/src/main/java/com/example/polity_correct/Vote.java
@@ -1,46 +1,36 @@
 package com.example.polity_correct;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.Timestamp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Vote extends AppCompatActivity {
 
     private FirebaseFirestore db;
 
     private TextView txt_proposition_title, getTxt_proposition_description;
-    private String user_id, proposition_key;
-    private int user_choice;
+    private String proposition_key;
+    private StatusVote user_choice;
     private double vote_grade;
     private Timestamp vote_date;
-    Map<String, Object> vote;
     private RadioGroup radioGroup;
     private RadioButton radioBtn;
     private RatingBar ratingbar;
-    private Button btnSend;
+    private User curr_user;
+    private Citizen curr_citizen;
 
 
     @Override
@@ -48,12 +38,12 @@ public class Vote extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vote);
 
+        curr_user = Login.getCurrUser();
+
         db = FirebaseFirestore.getInstance();
 
         TextView title = (TextView) findViewById(R.id.title_page);
         title.setText("הצבעה");
-
-        addListenerOnButton();
 
         txt_proposition_title = (TextView) findViewById(R.id.propositionTitle);
         getTxt_proposition_description = (TextView) findViewById(R.id.propositionDetails);
@@ -66,95 +56,43 @@ public class Vote extends AppCompatActivity {
         }
     }
 
-    //Extract user choice & Extract vote grade
-    private void addListenerOnButton() {
-        Intent intent = new Intent(this, HomeCitizen.class);
-
+    public void onClickSend(View view) {
         ratingbar = (RatingBar) findViewById(R.id.ratingBar);
         radioGroup = (RadioGroup) findViewById(R.id.radiobtns);
-        btnSend = (Button) findViewById(R.id.sendVote);
+        String rating = String.valueOf(ratingbar.getRating());
+        vote_grade = Double.parseDouble(rating);
 
-        btnSend.setOnClickListener(new View.OnClickListener() {
+        // get selected radio button from radioGroup
+        int selectedId = radioGroup.getCheckedRadioButtonId();
 
-            @Override
-            public void onClick(View v) {
+        // find the radiobutton by returned id
+        radioBtn = (RadioButton) findViewById(selectedId);
 
-                String rating = String.valueOf(ratingbar.getRating());
-                vote_grade = Double.parseDouble(rating);
-
-                // get selected radio button from radioGroup
-                int selectedId = radioGroup.getCheckedRadioButtonId();
-
-                // find the radiobutton by returned id
-                radioBtn = (RadioButton) findViewById(selectedId);
-
-                String vote_string = (String) radioBtn.getText();
-                switch (vote_string) {
-                    case "נגד":
-                        System.out.println("נגד");
-                        user_choice = 0;
-                        break;
-                    case "נמנע":
-                        System.out.println("נמנע");
-                        user_choice = 1;
-                        break;
-                    case "בעד":
-                        System.out.println("בעד");
-                        user_choice = 2;
-                        break;
-                    default:
-                        Toast.makeText(Vote.this,
-                                "ההצבעה לא תקינה", Toast.LENGTH_SHORT).show();
-                }
+        String vote_string = (String) radioBtn.getText();
+        switch (vote_string) {
+            case "נגד":
+                user_choice = StatusVote.against;
+                break;
+            case "נמנע":
+                user_choice = StatusVote.impossible;
+                break;
+            case "בעד":
+                user_choice = StatusVote.agreement;
+                break;
+            default:
                 Toast.makeText(Vote.this,
-                        "ההצבעה " + radioBtn.getText() + " נשלחה", Toast.LENGTH_SHORT).show();
+                        "ההצבעה לא תקינה", Toast.LENGTH_SHORT).show();
+        }
 
-                //Extract user id
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                assert user != null;
-                user_id = user.getUid();
+        //Extract vote date
+        Date currentTime = Calendar.getInstance().getTime();
+        vote_date = new Timestamp(currentTime);
+        curr_citizen = new Citizen(curr_user.getUserName(), curr_user.getPassword(), curr_user.getMail(), curr_user.getYearOfBirth(), curr_user.getGender(), UserType.citizen, curr_user.getKey_pg());
+        curr_citizen.Vote(curr_citizen, proposition_key, vote_grade, user_choice, vote_date);
 
-                //Extract vote date
-                Date currentTime = Calendar.getInstance().getTime();
-                vote_date = new Timestamp(currentTime);
-                updateDB();
-                startActivity(intent);
-            }
-        });
-    }
+        Intent intent = new Intent(this, HomeCitizen.class);
 
-    public void onClickSend(View view) {
-        //Login.user.Vote(proposition_key,vote_grade,user_choice,vote_date);
-    }
-
-    public void updateDB() {
-        // Add a new document with a generated ID
-        // Create a new user with a first and last name
-        vote = new HashMap<>();
-        vote.put("user_id", user_id);
-        vote.put("proposition_key", proposition_key);
-        vote.put("user_choice", user_choice);
-        vote.put("vote_grade", vote_grade);
-        vote.put("vote_date", vote_date);
-
-        db.collection("Votes")
-                .add(vote)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    private static final String TAG = " ";
-
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    private final Object TAG = " ";
-
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w((String) TAG, "Error adding document", e);
-                    }
-                });
+        Toast.makeText(Vote.this, "ההצבעה " + radioBtn.getText() + " נשלחה", Toast.LENGTH_SHORT).show();
+        startActivity(intent);
     }
 }
