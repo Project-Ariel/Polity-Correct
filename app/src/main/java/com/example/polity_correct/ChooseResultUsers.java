d0a2bfd2a4e81153fde354ab2661b521f2f1cac4
Reut Maslansky <rmaslansky@aternity.com>
2021-12-29 18:57:56 +0200

    Algorithm getResultsVotes per specific PG or of all users works good

--- app/src/main/java/com/example/polity_correct/ChooseResultUsers.java
+++ app/src/main/java/com/example/polity_correct/ChooseResultUsers.java
@@ -11,14 +11,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class ChooseResultUsers extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -27,7 +26,9 @@ public class ChooseResultUsers extends AppCompatActivity implements AdapterView.
     private ArrayList<String> titles = new ArrayList<>();
     private ArrayList<Proposition> propositions;
     private Proposition curr_proposition;
-    private int[] res = {0, 0, 0};
+    private int[] res;
+    private User currUser = Login.getCurrUser();
+    private String name_curr_pg, voteKeyPG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +51,6 @@ public class ChooseResultUsers extends AppCompatActivity implements AdapterView.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, titles);
         dropdown.setAdapter(adapter);
         dropdown.setOnItemSelectedListener(this);
-
     }
 
     public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
@@ -71,26 +71,92 @@ public class ChooseResultUsers extends AppCompatActivity implements AdapterView.
     }
 
     public void onClickSpecificPoliticalGroup(View view) {
-        Intent intent = new Intent(this, Statistics.class);
-        intent.putExtra("pg", "specific");
+        getVotesFromDBSpecificPG().addOnCompleteListener(task -> {
+            getNamePoliticalGroup().addOnCompleteListener(task0 -> {
+                Intent intent = new Intent(this, Statistics.class);
+                intent.putExtra("pg", name_curr_pg);
+                intent.putExtra("proposition_title", curr_proposition.getTitle());
+                intent.putExtra("result", res);
+                startActivity(intent);
+            });
+        });
+    }
 
-        startActivity(intent);
+    private Task<DocumentSnapshot> getNamePoliticalGroup() {
+        return FirebaseFirestore.getInstance().collection("PoliticalGroups")
+                .document(currUser.getKey_pg())
+                .get().addOnCompleteListener(task -> {
+                    name_curr_pg = task.getResult().get("group_name").toString();
+                });
     }
 
     private Task<QuerySnapshot> getVotesFromDB() {
+        res = new int[]{0, 0, 0};
         return FirebaseFirestore.getInstance().collection("Votes")
                 .whereEqualTo("proposition_key", curr_proposition.getKey())
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                long userChoice = (long) document.get("user_choice");
-                                res[(int) userChoice]++;
-
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String userChoice = (String) document.get("user_choice");
+                            int choice;
+                            switch (userChoice) {
+                                case "against":
+                                    choice = 0;
+                                    break;
+                                case "impossible":
+                                    choice = 1;
+                                    break;
+                                case "agreement":
+                                    choice = 2;
+                                    break;
+                                default:
+                                    throw new IllegalStateException("Unexpected value: " + userChoice);
                             }
+                            res[choice]++;
+                        }
+                    }
+                });
+    }
+
+    private Task<DocumentSnapshot> getKeyPoliticalGroup(String id_user) {
+        return FirebaseFirestore.getInstance().collection("Users")
+                .document(id_user)
+                .get().addOnCompleteListener(task -> {
+                    voteKeyPG = task.getResult().get("key_pg").toString();
+                });
+    }
+
+    private Task<QuerySnapshot> getVotesFromDBSpecificPG() {
+        res = new int[]{0, 0, 0};
+        return FirebaseFirestore.getInstance().collection("Votes")
+                .whereEqualTo("proposition_key", curr_proposition.getKey())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String voteUserId = (String) document.get("user_id");
+                            //get key_pg of user vote
+                            getKeyPoliticalGroup(voteUserId).addOnCompleteListener(task1 -> {
+                                if (voteKeyPG.equals(currUser.getKey_pg())) {
+                                    String userChoice = (String) document.get("user_choice");
+                                    int choice;
+                                    switch (userChoice) {
+                                        case "against":
+                                            choice = 0;
+                                            break;
+                                        case "impossible":
+                                            choice = 1;
+                                            break;
+                                        case "agreement":
+                                            choice = 2;
+                                            break;
+                                        default:
+                                            throw new IllegalStateException("Unexpected value: " + userChoice);
+                                    }
+                                    res[choice]++;
+                                }
+                            });
                         }
                     }
                 });
