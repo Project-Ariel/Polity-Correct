9bffc0dd0c6b17c076cdca9c9874998247982357
yaara <yaarabarak45620@gmail.com>
2021-12-29 14:49:17 +0200

    create signup activity to parialamet

---
+++ app/src/main/java/com/example/polity_correct/SignupPM.java
@@ -0,0 +1,21 @@
+package com.example.polity_correct;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SignupPM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_pm);
+    }
+
+    public void onClickOK(View view) {
+    }
+
+    public void onClickPMRegister(View view) {
+    }
+}
