5406765c7d1825cbacf61e4d3caa9eb1e390177f
yaara <yaarabarak45620@gmail.com>
2022-01-03 19:36:07 +0200

    Add menu to propositions_parliament.xml

--- app/src/main/java/com/example/polity_correct/PropositionsParliament.java
+++ app/src/main/java/com/example/polity_correct/PropositionsParliament.java
@@ -3,6 +3,7 @@ package com.example.polity_correct;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -14,6 +15,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
 
 public class PropositionsParliament extends AppCompatActivity {
@@ -21,6 +27,8 @@ public class PropositionsParliament extends AppCompatActivity {
     private ListView listView;
     private ArrayList<String> titles = new ArrayList<>();
     private static ArrayList<Proposition> propositions = new ArrayList<>();
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,46 @@ public class PropositionsParliament extends AppCompatActivity {
             listView.setAdapter(arrayAdapter);
         });
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView nav = (NavigationView) findViewById(R.id.navView);
+        nav.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.Home:
+                    startActivity(new Intent(this, HomeParliament.class));
+                case R.id.Statistics:
+                    Intent intent = new Intent(this, ChooseResultUsers.class);
+                    intent.putExtra("index_current_proposition", 0);
+                    startActivity(intent);
+                    break;
+                case R.id.Propositions:
+                    startActivity(new Intent(this, PropositionsParliament.class));
+                    break;
+                case R.id.UpdatePassword:
+                    startActivity(new Intent(this, Settings.class));
+                    break;
+                case R.id.LogOut:
+                    startActivity(new Intent(this, Login.class));
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     class listAdapter extends ArrayAdapter<String> {
