e2d04ef3ca6bdaca20dd28e38e352bbf7473c486
Reut Maslansky <rmaslansky@aternity.com>
2022-05-13 09:47:12 +0300

    Algo match_parliament

--- app/src/main/java/com/example/polity_correct/Algo.java
+++ app/src/main/java/com/example/polity_correct/Algo.java
@@ -1,6 +1,14 @@
 package com.example.polity_correct;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Algo {
     public static double[] calculate_votes_grades(ArrayList<String> votes, ArrayList<Double> grades) {
@@ -37,4 +45,57 @@ public class Algo {
             res[3] /= (sum);
         return res;
     }
+
+    //    Before sending this function, we should make sure that we have enough votes...
+    /*
+    For every parliament member, we would like to compare his or her voting vector with the citizen's voting vector,
+    with consideration of citizen's ranking of laws.
+    We will use this algorithm of diff between vectors:
+
+        diff= sum( ratings[i]*|vote_citizen[i]-vote_pm[i]| ) / sum( ratings )
+
+    We will take the value (1-diff) cause every grater rating will increase the diff...
+     */
+    public static LinkedHashMap<String, Integer> matchParliament(int[] votes_user, double[] rating, HashMap<String, int[]> votes_pm) {
+
+        HashMap<String, Integer> percents_result = new HashMap<>();
+        double sum_of_ratings = 0;
+
+
+        for (int r = 0; r < rating.length; r++) {
+            rating[r]++;
+            sum_of_ratings += rating[r];
+        }
+
+        for (Map.Entry<String, int[]> entry : votes_pm.entrySet()) {
+            double sum = 0;
+            for (int c = 0; c < votes_user.length; c++) {
+                sum += rating[c] * Math.abs(votes_user[c] - entry.getValue()[c]);
+            }
+            percents_result.put(entry.getKey(), (int)((1 - (sum / sum_of_ratings)) * 100));
+        }
+
+        //sort the hash-map for taking the best results.
+        return sortValues(percents_result);
+    }
+
+    //method to sort values
+    private static LinkedHashMap sortValues(HashMap map) {
+        List list = new LinkedList(map.entrySet());
+        //Descending Order
+        Collections.sort(list, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o2)).getValue()).compareTo(((Map.Entry) (o1)).getValue());
+            }
+        });
+        //copying the sorted list in HashMap to preserve the iteration order
+        LinkedHashMap sortedHashMap = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext(); ) {
+            Map.Entry entry = (Map.Entry) it.next();
+            sortedHashMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedHashMap;
+    }
 }
+
--- app/src/main/java/com/example/polity_correct/MatchParliament.java
+++ app/src/main/java/com/example/polity_correct/MatchParliament.java
@@ -24,6 +24,9 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class MatchParliament extends AppCompatActivity {
 
@@ -38,6 +41,12 @@ public class MatchParliament extends AppCompatActivity {
     private BarChart barChart;
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
+
+    static int[] votes_user;
+    static double[] rating;
+    static HashMap<String, int[]> votes_pm;
+    private LinkedHashMap<String, Integer> percents_result;
+//    private String[] best_parliaments;
 
 
     @Override
@@ -56,6 +65,7 @@ public class MatchParliament extends AppCompatActivity {
         matching_img = (ImageView) findViewById(R.id.matching_img);
         matching = (TextView) findViewById(R.id.matching_text);
 
+        // Handler for circular progress bar timer
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -68,12 +78,9 @@ public class MatchParliament extends AppCompatActivity {
                     handler.removeCallbacks(this);
                     circularProgressBar.setVisibility(View.GONE);
                     loading_text.setVisibility(View.GONE);
-                    matching.setVisibility(View.VISIBLE);
-                    matching_img.setVisibility(View.VISIBLE);
-                    parliament_text.setText("parliament");
-                    citizen_text.setText(Login.getCurrUser().getUserName());
-                    percent_text.setText("100%");
-                    showBarChart();
+
+                    show_result();
+
                 }
             }
         }, 30);
@@ -118,20 +125,74 @@ public class MatchParliament extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void show_result() {
+//        DB.setVotesForAlgo(votes_user, rating, votes_pm).addOnCompleteListener(task -> {
+
+        // manual just for test
+        votes_user = new int[]{0, 1, 0, 1, 2};
+        rating = new double[]{3, 4, 2, 5, 1};
+        votes_pm = new HashMap<>();
+        votes_pm.put("0WyASJFHH7eou7dLZnAnsuWnyK12", new int[]{0, 2, 1, 1, 1});
+        votes_pm.put("1HvhnuVmswM5nU1TJZz85SOeOdo2", new int[]{1, 2, 1, 2, 1});
+        votes_pm.put("1MOrTMUPcscNxpoyw95ajl7HNqE3", new int[]{0, 0, 0, 0, 0});
+        votes_pm.put("2Wou736KshQC8Hh6UbaSS3zeSip1", new int[]{1, 1, 1, 1, 1});
+        votes_pm.put("2jRqVEJIkQcJIqFMb7MNwANauz92", new int[]{0, 2, 0, 1, 2});
+        votes_pm.put("2xC4ldbBpQPn2XAnqXav5hMv0zD3", new int[]{2, 2, 2, 2, 2});
+        //
+
+        boolean flag = true;
+        for (int i = 0; i < votes_user.length; i++) {
+            if (votes_user[i] == -1) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            percents_result = Algo.matchParliament(votes_user, rating, votes_pm);
+            String first_pm = percents_result.keySet().iterator().next();
+            Integer percent_best_pm = percents_result.get(first_pm);
+            User best_pm = new User(first_pm);
+            DB.getUser(best_pm).addOnCompleteListener(task0 -> {
+                parliament_text.setText(best_pm.getUserName());
+                percent_text.setText(percent_best_pm.toString()+"%");
+                matching.setVisibility(View.VISIBLE);
+                matching_img.setVisibility(View.VISIBLE);
+                citizen_text.setText(Login.getCurrUser().getUserName());
+                showBarChart();
+            });
+        } else {
+            loading_text.setText("לצערנו אין מספיק הצבעות שלך על חוקים שהצובעו על ידי חברי כנסת..\n\nגש להצביע");
+            loading_text.setVisibility(View.VISIBLE);
+        }
+//        });
+    }
+
+//    public void prepareDataBarChart() {
+//        parliaments = new ArrayList<>();
+//        for (Map.Entry<String, Double> it : percents_result.entrySet()) {
+//            User pm = new User(it.getKey());
+//            DB.getUser(pm).addOnCompleteListener(task -> {
+//                parliaments.add(pm.getUserName());
+//            });
+//        }
+//    }
+
     public void showBarChart() {
         barChart = (BarChart) findViewById(R.id.barchart);
-        barChart.setVisibility(View.VISIBLE);
 
         ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0f, 20, "לפיד"));
-        entries.add(new BarEntry(1f, 60, "בנט"));
-        entries.add(new BarEntry(2f, 40, "ליצמן"));
-        entries.add(new BarEntry(3f, 80, "סמוטריץ'"));
-        entries.add(new BarEntry(4f, 100, "ביבי"));
+        int count = 0;
+        for (Map.Entry<String, Integer> it : percents_result.entrySet()) {
+            if (count < 5) {
+                entries.add(new BarEntry((float) count, it.getValue(), it.getKey()));
+            }
+            count++;
+        }
+        barChart.setVisibility(View.VISIBLE);
 
-        String[] PARLIAMENT= new String[5];
-        for(BarEntry e:entries){
-            PARLIAMENT[(int)e.getX()]=(String) e.getData();
+
+        String[] PARLIAMENT = new String[5];
+        for (BarEntry e : entries) {
+            PARLIAMENT[(int) e.getX()] = (String) e.getData();
         }
         BarDataSet bardataset = new BarDataSet(entries, "התפלגות ההתאמות");
 
