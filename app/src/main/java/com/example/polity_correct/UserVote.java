8886633d4b58dd0bd10c131644862628986f894f
yaara <yaarabarak45620@gmail.com>
2022-05-16 17:33:56 +0300

    create UserVote obj and comparator for sorting the user vote

---
+++ app/src/main/java/com/example/polity_correct/UserVote.java
@@ -0,0 +1,82 @@
+package com.example.polity_correct;
+
+import java.util.Comparator;
+
+public class UserVote{
+    String rule_id; // rule id
+    int vote;
+    Double rate;
+    Boolean accepted; //proposition accepted already
+
+
+    public UserVote(String rule_id, String vote, Double rate) {
+        this.rule_id = rule_id;
+        if( vote.equals("abstain"))
+            this.vote = 1;
+        else
+            if( vote.equals("against"))
+                this.vote = 0;
+            else
+                this.vote=2;
+        this.rate = rate;
+    }
+
+    public String getRule_id() {
+        return rule_id;
+    }
+
+    public void setRule_id(String rule_id) {
+        this.rule_id = rule_id;
+    }
+
+    public int getVote() {
+        return vote;
+    }
+
+    public void setVote(int vote) {
+        this.vote = vote;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+
+    public Boolean getAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(Boolean accepted) {
+        this.accepted = accepted;
+    }
+
+    @Override
+    public String toString() {
+        return "UserVote{" +
+                "rule_id='" + rule_id + '\'' +
+                ", vote=" + vote +
+                ", rate=" + rate +
+                ", accepted=" + accepted +
+                '}';
+    }
+}
+
+class SortByGrade implements Comparator<UserVote> {
+
+    // Method
+    // Sorting in ascending order of roll number
+    public int compare(UserVote a, UserVote b)
+    {
+        if(a.rate==b.rate)
+            return 0;
+        else
+        if(a.rate<b.rate)
+            return 1;
+        else
+            return -1;
+    }
+}
+
